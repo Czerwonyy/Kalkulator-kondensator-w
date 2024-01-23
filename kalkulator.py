b01ca3b76e7ea92e920bb10204cbab3a56813419
Czerwonyy <czerwony.public@gmail.com>
2024-01-23 15:07:31 +0100

    Update kalkulator.py
    dodano jednostke "F (farad)" do wyniku końcowego

--- kalkulator.py
+++ kalkulator.py
@@ -11,7 +11,7 @@ if question == "1":                             # Obliczanie równoległe
     print("podaj wartość 2 kondensatora: ")
     b = int(input())
     
-    print(a + b)
+    print(a + b,"F")
     
 if question == "2":                             # Obliczanie szeregowe
     print("podaj wartość 1 kondensatora: ")
@@ -21,6 +21,6 @@ if question == "2":                             # Obliczanie szeregowe
     
     c = (1/a) + (1/b)
     
-    print(1/c)
+    print(1/c,"F")
     
 # Cześć, jestem kodem :D
